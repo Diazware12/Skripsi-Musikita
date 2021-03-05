1a3d673c8434dc43d4d922c0318da1e9a6be2a58
diazware12 <diazwalker98@gmail.com>
2021-03-05 19:11:52 +0700

    first init

---
+++ Skripsi/Skripsi/views.py
@@ -0,0 +1,21 @@
+from django.http import HttpResponse
+from django.shortcuts import render 
+
+def dashboard (request):
+    return render(request,'dashboard.html')
+
+def rating (request):
+    return render(request,'rating.html')
+
+def registerMember (request):
+    return render(request,'registerMember.html')
+
+def registerMusicStore (request):
+    return render(request,'registerMusicStore.html')
+
+def profile (request):
+    return render(request,'profile.html')
+
+def profileMusicStore (request):
+    return render(request,'profileMusicStore.html')
+    
