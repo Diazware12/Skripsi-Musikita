e29e38feebd4d0930c9a16a6801cbb1655afd658
diazware12 <diazwalker98@gmail.com>
2021-03-21 09:46:36 +0700

    edit UI

--- Skripsi/static/vendor2/scripts/custom.js
+++ Skripsi/static/vendor2/scripts/custom.js
@@ -7,3 +7,20 @@ for (let i = 0; i < star.length; i++) {
         showValue.innerHTML = "Thank You";
 	});
 }
+
+$(document).on('click', '.dropdown-menu', function (e) {
+	e.stopPropagation();
+  });
+  
+  // make it as accordion for smaller screens
+  if ($(window).width() < 992) {
+	$('.dropdown-menu a').click(function(e){
+	  e.preventDefault();
+		if($(this).next('.submenu').length){
+		  $(this).next('.submenu').toggle();
+		}
+		$('.dropdown').on('hide.bs.dropdown', function () {
+	   $(this).find('.submenu').hide();
+	})
+	});
+  }
