eda13becb181f9f96264931c6cffae15fea94fd8
diazware12 <diazwalker98@gmail.com>
2021-06-09 15:41:33 +0700

    nambah fitur register admin @CruzerBL

--- profileUser/urls.py
+++ profileUser/urls.py
@@ -4,9 +4,12 @@ from . import views
 from django.conf import settings
 
 urlpatterns = [
+   path('usercontrol',views.userControl,name="usercontrol"),
+   path('usercontrol/<auth_token>/delete',views.deleteUser,name="deleteUser"),
    path('<userName>',views.profilePage,name="profilePage"),
    path('<userName>/edit/profilePicture',views.editProfilePicture,name="editProfilePicture"),
    path('<userName>/edit/userData',views.editUserData,name="editUserData"),
+   
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
